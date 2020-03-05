3dd119b0e1ded81f820e212bb87c3a1c32f86a46
limor <limorBortman@gmail.com>
2020-03-05 11:10:32 -0800

    Add MaterializingStreamsEx

---
+++ src/main/scala/part2primer/ex/MaterializingStreamsEx.scala
@@ -0,0 +1,40 @@
+package part2primer.ex
+
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
+import infra.Implicits._
+
+object MaterializingStreamsEx extends App {
+
+  /**
+   * 1. Return the last element out of a source
+   */
+  Source(1 to 10)
+    .runWith(Sink.last)
+    .foreach(println)
+
+  /**
+   * 2.Compute the total word count out of a stream of sentences
+   */
+  val sentenceSource = Source(List("I am the one.", "No I am the one", "No, it is I."))
+  //Op1:
+  sentenceSource
+  .map(countWords)
+  .reduce((a, b) => a + b)
+  .runForeach(println)
+
+  //Op2:
+  sentenceSource
+    .toMat(Sink.fold(0)((currentWordsCont, newSentence) => currentWordsCont + countWords(newSentence)))(Keep.right) //This solution == to sentenceSource.runWith(Sink.fold(0)((currentWordsCont, newSentence) => currentWordsCont + countWords(newSentence))))
+    .run()
+    .foreach(println)
+
+  //Op3:
+  sentenceSource
+    .via(Flow[String].fold(0)((currentWordsCont, newSentence) => currentWordsCont + countWords(newSentence)))
+    .runWith(Sink.head)
+    .foreach(println)
+
+
+  def countWords(string: String): Int = string.split(" ").length
+
+}
